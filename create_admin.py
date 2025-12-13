25b29a33b3e1ce89f6a00e4719f76168f2fa4658
Altan Tarı <altantari@gmail.com>
2025-12-13 05:41:21 +0300

    Geçici admin oluşturma scripti eklendi

---
+++ create_admin.py
@@ -0,0 +1,8 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "sifre1234")
+    print("Admin kullanıcı oluşturuldu!")
+else:
+    print("Admin zaten var.")
